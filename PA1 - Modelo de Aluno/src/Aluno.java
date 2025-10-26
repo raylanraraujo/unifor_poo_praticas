9ae5e4ff6ff128c8e7f5eb16737377a7acbc42f9
Raylan Araujo <raylanra@gmail.com>
2025-10-26 09:17:51 -0300

    criado o PA1

---
+++ PA1 - Modelo de Aluno/src/Aluno.java
@@ -0,0 +1,9 @@
+public class Aluno {
+    private String nome;
+    private int matricula;
+    private String curso;
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
